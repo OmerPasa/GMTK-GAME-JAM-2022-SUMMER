23dc7ec2dec066a73cb4bbe69afdcbd5d3748cf3
Ömer Faruk Çalışkan <omerfa33@gmail.com>
2024-07-07 20:38:11 +0300

    last bug fix for this game
    animation fixed for idle

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -8,36 +8,34 @@ public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 
 public class BattleSystem : MonoBehaviour
 {
-	public GameObject DropzoneObject;
-	public GameObject Hand;
-	public List<GameObject> Cards = new List<GameObject>();
+    public GameObject DropzoneObject;
+    public GameObject Hand;
+    public List<GameObject> Cards = new List<GameObject>();
 
-	public GameObject playerPrefab;
-	public GameObject enemyPrefab;
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
 
-	public Transform playerBattleStation;
-	public Transform enemyBattleStation;
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
 
+    Unit playerUnit;
+    Unit enemyUnit;
 
-	Unit playerUnit;
-	Unit enemyUnit;
+    public Text dialogueText;
 
-	public Text dialogueText;
+    public BattleHUD playerHUD;
+    public BattleHUD enemyHUD;
 
-	public BattleHUD playerHUD;
-	public BattleHUD enemyHUD;
+    public BattleState state;
 
-	public BattleState state;
-
-	public AudioSource animationsounds;
+    public AudioSource animationsounds;
     public AudioClip[] hasarsoundArray;
-	public AudioClip dealcard;
+    public AudioClip dealcard;
 
-// NEEDS İMPLEMENTATİONS!!!!!!
-
-	private Animator animator;
-    private string currentAnimaton;
-	const string PLAYER_IDLE = "Player_Idle";
+    // Animator for the player object
+    private Animator playerAnimator;
+    private string currentAnimation;
+    const string PLAYER_IDLE = "Player_Idle";
     const string PLAYER_DODGEBACKWARD = "Player_DodgeBackward";
     const string PLAYER_DODGEFORWARD = "Player_DodgeForward";
     const string PLAYER_RUN = "Player_Run";
@@ -46,157 +44,159 @@ public class BattleSystem : MonoBehaviour
     const string PLAYER_AIR_ATTACK = "Player_Jump_Firing";
     const string PLAYER_DEATH = "Player_Death";
     const string PLAYER_TAKEDAMAGE = "Player_TakeDamage";
-	private bool isAttacking;
+    private bool isAttacking;
     private bool isTakingDamage;
     private bool isDying;
     private bool isntDead;
 
     void Start()
     {
-		//ChangeAnimationState(PLAYER_IDLE);
-		AudioSource animationsounds = GameObject.Find("animationsounds").GetComponent<AudioSource>();
+        AudioSource animationsounds = GameObject.Find("animationsounds").GetComponent<AudioSource>();
 
-		isntDead = true;
-		state = BattleState.START;
-		StartCoroutine(SetupBattle());
-		animator = GetComponent<Animator>();
+        isntDead = true;
+        state = BattleState.START;
+        StartCoroutine(SetupBattle());
     }
 
-	IEnumerator SetupBattle()
-	{
-		GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
-		playerUnit = playerGO.GetComponent<Unit>();
-
-		GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
-		enemyUnit = enemyGO.GetComponent<Unit>();
-
-		dialogueText.text = "A wild " + enemyUnit.unitName + " approaches...";
-
-		playerHUD.SetHUD(playerUnit);
-		enemyHUD.SetHUD(enemyUnit);
-
-
-		yield return new WaitForSeconds(2f);
-		
-		state = BattleState.PLAYERTURN;
-		PlayerTurn();
-	}
-
-	public IEnumerator PlayerAttack(int BaseDamage)
-	{
-		ChangeAnimationState(PLAYER_ATTACK);
-		bool isDead = enemyUnit.TakeDamage(BaseDamage);
-		Debug.Log (BaseDamage + " amount of damage is given");
-		enemyHUD.SetHP(enemyUnit.currentHP);
-		animationsounds.clip = hasarsoundArray[Random.Range(0,hasarsoundArray.Length)];
-		animationsounds.Play();
-
-		dialogueText.text = "The attack is successful!";
-
-		yield return new WaitForSeconds(1f);
-		Debug.Log("waited");
-		if(isDead)
-		{
-			state = BattleState.WON;
-			EndBattle();
-		} else
-		{
-			state = BattleState.ENEMYTURN;
-			StartCoroutine(EnemyTurn());
-		}
-	}
-
-	IEnumerator EnemyTurn()
-	{
-		animationsounds.clip = hasarsoundArray[Random.Range(0,hasarsoundArray.Length)];
-		animationsounds.Play();
-		dialogueText.text = enemyUnit.unitName + " attacks!";
-
-		yield return new WaitForSeconds(1f);
-
-		bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
-
-
-		playerHUD.SetHP(playerUnit.currentHP);
-
-		yield return new WaitForSeconds(1f);
-
-		if(isDead)
-		{
-			state = BattleState.LOST;
-			EndBattle();
-		} else
-		{
-			state = BattleState.PLAYERTURN;
-			DropzoneObject.SetActive (true);
-			PlayerTurn();
-		}
-
-	}
-
-	void EndBattle()
-	{
-		if(state == BattleState.WON)
-		{
-			dialogueText.text = "You won the battle!";
-		} else if (state == BattleState.LOST)
-		{
-			dialogueText.text = "You were defeated.";
-		}
-	}
-
-	void PlayerTurn()
-	{
-		dialogueText.text = "Choose an action:";
-	}
-
-	IEnumerator PlayerHeal(int hp)
-	{
-		playerUnit.Heal(hp);
-
-		playerHUD.SetHP(playerUnit.currentHP);
-		dialogueText.text = "You feel renewed strength!";
-
-		yield return new WaitForSeconds(2f);
-
-		state = BattleState.ENEMYTURN;
-		StartCoroutine(EnemyTurn());
-	}
-
-	public void OnAttackButton(int Dam)
-	{
-		if (state != BattleState.PLAYERTURN)
-			return;
-
-		StartCoroutine(PlayerAttack(Dam));
-	}
-
-	public void OnHealButton(int hp)
-	{
-		if (state != BattleState.PLAYERTURN)
-			return;
-
-		StartCoroutine(PlayerHeal(hp));
-	}
-
-	public void OnDrawCard()
-	{
-		animationsounds.clip = dealcard;
-		animationsounds.Play();
-		GameObject randCard = Cards[Random.Range(0, Cards.Count)];
-		Transform parent = Hand.transform;
-
-		if (Hand.transform.childCount < 6)
-		{
-			randCard.transform.SetParent(parent, false);
-		}
-	}
-
-	    void ChangeAnimationState(string newAnimation)
+    IEnumerator SetupBattle()
     {
-        if (currentAnimaton == newAnimation) return;
+        GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+        playerUnit = playerGO.GetComponent<Unit>();
+        playerAnimator = playerGO.GetComponent<Animator>(); // Get the Animator component from the player object
 
-        animator.Play(newAnimation);
-        currentAnimaton = newAnimation;
+        GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
+        enemyUnit = enemyGO.GetComponent<Unit>();
+
+        dialogueText.text = "A wild " + enemyUnit.unitName + " approaches...";
+
+        playerHUD.SetHUD(playerUnit);
+        enemyHUD.SetHUD(enemyUnit);
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
+    }
+
+    public IEnumerator PlayerAttack(int BaseDamage)
+    {
+        ChangeAnimationState(PLAYER_ATTACK);
+        bool isDead = enemyUnit.TakeDamage(BaseDamage);
+        Debug.Log(BaseDamage + " amount of damage is given");
+        enemyHUD.SetHP(enemyUnit.currentHP);
+        animationsounds.clip = hasarsoundArray[Random.Range(0, hasarsoundArray.Length)];
+        animationsounds.Play();
+
+        dialogueText.text = "The attack is successful!";
+
+        yield return new WaitForSeconds(1f);
+        Debug.Log("waited");
+        ChangeAnimationState(PLAYER_IDLE);
+
+        if (isDead)
+        {
+            state = BattleState.WON;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        animationsounds.clip = hasarsoundArray[Random.Range(0, hasarsoundArray.Length)];
+        animationsounds.Play();
+        dialogueText.text = enemyUnit.unitName + " attacks!";
+
+        yield return new WaitForSeconds(1f);
+
+        bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+        playerHUD.SetHP(playerUnit.currentHP);
+
+        yield return new WaitForSeconds(1f);
+
+        ChangeAnimationState(PLAYER_IDLE);
+        if (isDead)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.PLAYERTURN;
+            DropzoneObject.SetActive(true);
+            PlayerTurn();
+        }
+    }
+
+    void EndBattle()
+    {
+        if (state == BattleState.WON)
+        {
+            dialogueText.text = "You won the battle!";
+        }
+        else if (state == BattleState.LOST)
+        {
+            dialogueText.text = "You were defeated.";
+        }
+    }
+
+    void PlayerTurn()
+    {
+        dialogueText.text = "Choose an action:";
+    }
+
+    IEnumerator PlayerHeal(int hp)
+    {
+        playerUnit.Heal(hp);
+
+        playerHUD.SetHP(playerUnit.currentHP);
+        dialogueText.text = "You feel renewed strength!";
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.ENEMYTURN;
+        StartCoroutine(EnemyTurn());
+    }
+
+    public void OnAttackButton(int Dam)
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+
+        StartCoroutine(PlayerAttack(Dam));
+    }
+
+    public void OnHealButton(int hp)
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+
+        StartCoroutine(PlayerHeal(hp));
+    }
+
+    public void OnDrawCard()
+    {
+        animationsounds.clip = dealcard;
+        animationsounds.Play();
+        GameObject randCard = Cards[Random.Range(0, Cards.Count)];
+        Transform parent = Hand.transform;
+
+        if (Hand.transform.childCount < 6)
+        {
+            randCard.transform.SetParent(parent, false);
+        }
+    }
+
+    void ChangeAnimationState(string newAnimation)
+    {
+        if (currentAnimation == newAnimation) return;
+
+        playerAnimator.Play(newAnimation); // Use the player's Animator component
+        currentAnimation = newAnimation;
     }
 }
